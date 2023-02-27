5737cd239dc4db8c55e9223952304d7cf8697913
GeronXXX <lityagin.ru@gmail.com>
2023-02-27 23:07:51 +0300

    commit_2

--- src/test/java/AutomationPractice.java
+++ src/test/java/AutomationPractice.java
@@ -1,4 +1,3 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,7 +7,7 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class automationPractice {
+public class AutomationPractice {
     @AfterAll
     static void after() {Configuration.holdBrowserOpen = true;
     }
@@ -22,6 +21,17 @@ public class automationPractice {
         $("#wiki-tab").click();
         $("#wiki-pages-box").$(".js-wiki-more-pages-link").click();
         $(By.linkText("SoftAssertions")).click();
-        $("#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+        $("#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"),
+                text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "@Test\n" +
+                "void test() {\n" +
+                "Configuration.assertionMode = SOFT;\n" +
+                "open(\"page.html\");\n" +
+                "\n" +
+                "$(\"#first\").should(visible).click();\n" +
+                "$(\"#second\").should(visible).click();\n" +
+                "}\n" +
+                "}"));
     }
 }
